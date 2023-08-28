b68260e3ae13e98ee2f05ec8ddd9f0a0259baa2f
zJimpi <jimpidesk@gmail.com>
2023-08-28 22:00:37 +0530

    room and seat allocation class

---
+++ src/main/java/com/pattern/ExamSeatingPatternGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSeatingPatternGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSeatingPatternGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pattern/entity/Room.java
@@ -0,0 +1,5 @@
+package com.pattern.entity;
+
+public class Room {
+
+}
