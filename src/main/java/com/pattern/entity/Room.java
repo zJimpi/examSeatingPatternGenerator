e01504389291c491b452983ca76de138a0529b7b
zJimpi <jimpidesk@gmail.com>
2023-08-28 22:38:32 +0530

    variables are declared

--- src/main/java/com/pattern/entity/Room.java
+++ src/main/java/com/pattern/entity/Room.java
@@ -1,5 +1,33 @@
 package com.pattern.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="room_details")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Room {
 
+	@Id
+	private int roomNo;
+	
+	@Column(nullable = false)
+	private int noOfSeats;
+	
+	private int startRollNo;
+	
+	private int endRollNo;
+	
 }
--- src/main/java/com/pattern/entity/SeatAllocation.java
+++ src/main/java/com/pattern/entity/SeatAllocation.java
@@ -1,5 +1,22 @@
 package com.pattern.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class SeatAllocation {
 
+	@Id
+	private int seatNo;
 }
